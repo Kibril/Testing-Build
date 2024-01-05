5770ae5b614bf69997fb1c4639cd8a7f7f924f87
Kibril <wegoodm8@gmail.com>
2024-01-05 20:56:00 +0300

    123

--- kubejs/server_scripts/custom_create_recipes_mixin.js
+++
@@ -1,9 +0,0 @@
-onEvent("recipes", (event) => {
-  // Change recipes here
-  event.recipes
-    .createMixing("minecraft:honeycomb", [
-      "#leaves",
-      Fluid.of("create:honey", 250),
-    ])
-    .heated();
-});
--- kubejs/startup_scripts/worldgen_remove.js
+++
@@ -1,9 +0,0 @@
-onEvent("worldgen.remove", (event) => {
-  event.removeOres((ores) => {
-    ores.blocks = [
-      "majruszsdifficulty:enderium_shard_ore",
-      "biomesoplenty:small_rose_quartz_bud",
-      "biomesoplenty:_rose_quartz_block",
-    ];
-  });
-});
